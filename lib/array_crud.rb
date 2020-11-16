d61547cb5a67dc180c5a90c408d4ded44b014d67
Kalynn <k.mac91@icloud.com>
2020-11-15 20:25:30 -0600

    commit tree 53cf34a6694cc73d11bf2cb0e3378cccdcfe532a

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,47 @@
 def create_an_empty_array
-  
+  my_array = []
 end
 
 def create_an_array
+    cheese = ["cheddar", "nacho", "white", "yellow"]
   
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    cheese = ["cheddar", "nacho", "white", "yellow", "blue"]
+    cheese << "arrays!"
+    
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    cheese = ["cheddar", "nacho", "white", "yellow", "blue"]
+    cheese.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+    cheese = ["cheddar", "nacho", "white", "yellow", "arrays!"]
+    array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    cheese = ["cheddar", "nacho", "white", "yellow", "arrays!"]
+    array.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
+    cheese = ["am", "nacho", "white", "yellow", "arrays!"]
+    cheese[0]
   
 end
 
 def retrieve_first_element_from_array(array)
+    cheese = ["wow", "nacho", "white", "yellow", "arrays!"]
+    cheese[0]
   
 end
 
 def retrieve_last_element_from_array(array)
+    cheese = ["cheddar", "nacho", "white", "yellow", "arrays!"]
+    cheese[4]
   
 end
